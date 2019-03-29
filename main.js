e40ef1e27c38198a0f3022379c2254f4ca039a25
thainq_uet <thainq00@gmail.com>
2019-03-29 09:22:45 +0700

    embedded 6 iframe

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.set('PORT', process.env.PORT || 7777);
 
 const dummyData = require('./HistoryFilterPageDemoData')
 let dataFake = dummyData
-app.get('/', (req, res) => res.send(dataFake))
+// app.get('/', (req, res) => res.send(dataFake))
+
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+ '/public/index.html')
+})
 
 app.listen(app.get('PORT'), () => console.log('success'))
